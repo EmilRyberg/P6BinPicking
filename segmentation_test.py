e818aa7c024180fe0948532c2a2733c2c4a29d10
Emil Albin Ryberg <eryber17@student.aau.dk>
2020-04-21 11:24:51 +0200

    Added detectron2 instance segmentation model to code

---
+++ segmentation_test.py
@@ -0,0 +1,13 @@
+from vision.segmentation.detector import InstanceDetector
+import cv2
+import numpy as np
+
+if __name__ == "__main__":
+    detector = InstanceDetector('model_final_0-002LR.pth')
+    img = cv2.imread('color1582022679.966812-0.png')
+    results = detector.predict(img)
+    #["PCB", "BottomCover", "BlueCover", "WhiteCover", "BlackCover"]
+    mask = results["instances"].pred_masks[0].cpu().numpy()
+    print(results["instances"].pred_classes[0])
+    print(mask * 255)
+    cv2.imwrite('box.jpg', mask * 255)
---
+++ vision/segmentation/detector.py
@@ -0,0 +1,32 @@
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+
+
+class InstanceDetector:
+    def __init__(self, weights_path):
+        self.cfg = self.__setup_cfg(weights_path)
+        self.predictor = DefaultPredictor(self.cfg)
+
+    def predict(self, img):
+        """
+        Args:
+            img (np.ndarray): an image of shape (H, W, C) (in BGR order).
+
+        Returns:
+            predictions (dict)
+
+        classes should be: ["PCB", "BottomCover", "BlueCover", "WhiteCover", "BlackCover"]
+        """
+        return self.predictor(img)
+
+    def __setup_cfg(self, weights_path):
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file("Misc/cascade_mask_rcnn_R_50_FPN_1x.yaml"))
+        cfg.MODEL.WEIGHTS = weights_path
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
+        cfg.MODEL.RETINANET.SCORE_THRESH_TEST = 0.5
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = 0.5
+        cfg.freeze()
+        return cfg
