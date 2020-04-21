603609b4e38e2a30e9421cef2e8e1e0d2afe4e62
Albert <asol17@student.aau.dk>
2020-04-21 14:01:32 +0200

    Surface normal and mask handling

--- vision/vision.py
+++ vision/vision.py
@@ -1,23 +1,22 @@
-from vision.yolo.detector import Detector
+from darknetpy.detector import Detector
 import pyrealsense2 as rs
 from PIL import Image as pimg
 from PIL import ImageDraw
 import cv2
 import glob
-import imutils 
+import imutils
 import numpy as np
 import scipy.misc
-from controller.enums import PartEnum, OrientationEnum
+from enums import PartEnum, OrientationEnum
 import os
-from OrientationCNN.orientation_detector import OrientationDetector
-from controller.class_converter import convert_to_part_id, convert_from_part_id
+from orientation_detector import OrientationDetector
+from class_converter import convert_to_part_id
 from utils import image_shifter
 from aruco import Calibration
+from surface_normal import SurfaceNormals
 
-import os
 
-
-YOLOCFGPATH = 'vision/yolo/'
+YOLOCFGPATH = '/DarkNet/'
 IMAGE_NAME = "webcam_capture.png"
 ORIENTATION_MODEL_PATH = "orientation_cnn.hdf5"
 
@@ -28,9 +27,7 @@ class Vision:
         yolo_cfg_path_absolute = self.current_directory + YOLOCFGPATH
         self.image_path = self.current_directory + "/" + IMAGE_NAME
         self.mask_path = self.current_directory + "/masks/"
-        self.detector = Detector(os.path.join(yolo_cfg_path_absolute, 'cfg/obj.data'),
-                                 os.path.join(yolo_cfg_path_absolute, 'cfg/yolov3-tiny.cfg'),
-                                 os.path.join(yolo_cfg_path_absolute, 'yolov3-tiny_final.weights'))
+        self.detector = Detector(yolo_cfg_path_absolute + 'cfg/obj.data', yolo_cfg_path_absolute + 'cfg/yolov3-tiny.cfg', yolo_cfg_path_absolute + 'yolov3-tiny_final.weights')
         self.counter = 0
         self.first_run = True
         self.results = None
@@ -53,8 +50,8 @@ class Vision:
             rgb_camera = sensors[1]
             rgb_camera.set_option(rs.option.white_balance, 4600)
             rgb_camera.set_option(rs.option.exposure, 80)
-            rgb_camera.set_option(rs.option.saturation, 65)
-            rgb_camera.set_option(rs.option.contrast, 50)
+            #rgb_camera.set_option(rs.option.saturation, 65)
+            #rgb_camera.set_option(rs.option.contrast, 50)
 
 
             frames = None
@@ -77,15 +74,18 @@ class Vision:
         part = (-1, -1, -1, -1, -1)
         # result is an array of dictionaries
         found_class_index = 0
-        for x1, y1, x2, y2, conf, cls_conf, cls_pred in self.results:
-            if (class_id1 == cls_pred or class_id2 == cls_pred) and cls_conf > 0.6:
+        for i in range(len(self.results)):
+            d = self.results[i]
+            if (d['class'] == class_id1 or d['class'] == class_id2) and d['prob'] > 0.6:
                 if fuse_index > -1 and fuse_index != found_class_index:
                     found_class_index += 1
                     continue
-                width = x2 - x1
-                height = y2 - y1
-                x_coord = width / 2 + x1
-                y_coord = height / 2 + y1
+                part_class = d['class']
+                prob = d['prob']
+                width = d['right'] - d['left']
+                height = d['bottom'] - d['top']
+                x_coord = width / 2 + d['left']
+                y_coord = height / 2 + d['top']
                 if height > width:
                     orientation = OrientationEnum.VERTICAL.value
                     grip_width = width * 0.58
@@ -96,28 +96,30 @@ class Vision:
                     orientation = OrientationEnum.HORIZONTAL.value
                     grip_width = height * 0.58
                     print("[W] Could not determine orientation, using 1 as default")
-                #new_part_id = convert_to_part_id(part_class)
-                part = (cls_pred, x_coord, y_coord, orientation, grip_width)
+                new_part_id = convert_to_part_id(part_class)
+                part = (new_part_id, x_coord, y_coord, orientation, grip_width)
                 break
         print(part)
         return part
 
     def detect_object(self):
-        np_img = pimg.open(self.image_path)
-        self.results = self.detector.predict(np_img)
+        results = self.detector.detect(self.image_path)
         self.draw_boxes(self.results)
 
     def draw_boxes(self, results):
         source_img = pimg.open(self.image_path).convert("RGBA")
-        for x1, y1, x2, y2, conf, cls_conf, cls_pred in self.results:
-            if cls_conf > 0.6:
-                width = x2 - x1
-                height = y2 - y1
-                x_coord = width / 2 + x1
-                y_coord = height / 2 + y1
+        for i in range(len(results)):
+            d = results[i]
+            if d['prob'] > 0.6:
+                classify = d['class']
+                prob = d['prob']
+                width = d['right'] - d['left']
+                height = d['bottom'] - d['top']
+                x_coord = width / 2 + d['left']
+                y_coord = height / 2 + d['top']
                 draw = ImageDraw.Draw(source_img)
-                draw.rectangle(((x1, y1), (x2, y2)), fill=None, outline=(200, 0, 150), width=6)
-                draw.text((x_coord, y_coord), convert_from_part_id(int(cls_pred)))
+                draw.rectangle(((d['left'], d['top']), (d['right'], d['bottom'])), fill=None, outline=(200, 0, 150), width=6)
+                draw.text((x_coord, y_coord), d['class'])
         source_img.save('boundingboxes.png')
 
     def is_facing_right(self, np_image):
@@ -157,58 +159,6 @@ class Vision:
             #cv2.waitKey(0)
             return cX, cY
 
-    def get_z(self, x, y, depth_image):
-        z = depth_image[x,y,0]
-        print(z)
-        return z
-
-    def vector_normal(self, x, y, mask, depthimage):
-        A = np.array([x, y, self.get_z(x, y, depthimage)])
-        B = np.array([x-10, y-10, self.get_z(x-10, y-10, depthimage)])
-        C = np.array([x+10, y-10, self.get_z(x+10, y-10, depthimage)])
-        D = np.array([x-10, y+10, self.get_z(x-10, y+10, depthimage)])
-
-
-        vector1 = D-B
-        vector2 = C-B
-        normal_vector = np.cross(vector1, vector2)
-        print(normal_vector)
-        """
-        #DEBUG CODE FOR VISUALISATION
-        a, b, c = normal_vector
-
-        # This evaluates a * x3 + b * y3 + c * z3 which equals d
-        d = np.dot(normal_vector, D)
-
-        print('The equation is {0}x + {1}y + {2}z = {3}'.format(a, b, c, d))
-
-        import matplotlib.pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-
-        x = np.linspace(B[0], C[0], D[0])
-        y = np.linspace(B[1], C[1], D[1])
-        X, Y = np.meshgrid(x, y)
-
-        Z = (d - a * X - b * Y) / c
-
-        # plot the mesh. Each array is 2D, so we flatten them to 1D arrays
-        ax.plot(X.flatten(),
-        Y.flatten(),
-        Z.flatten(), 'bo ')
-
-        # plot the original points. We use zip to get 1D lists of x, y and z
-        # coordinates.
-        ax.plot(*zip(B, C, D), color='r', linestyle=' ', marker='o')
-        vector_origin = B
-        ax.quiver(vector_origin[0], vector_origin[1], vector_origin[2], normal_vector[0], normal_vector[1], normal_vector[2])
-        # adjust the view so we can see the point/plane alignment
-        ax.view_init(10, 20)
-        plt.tight_layout()
-        #plt.savefig('images/plane.png')
-        plt.show()"""
-
     def find_part_for_grasp(self):
         masks = glob.glob(self.mask_path + "*")
         number_of_masks = len(masks)
@@ -231,7 +181,6 @@ class Vision:
         print(part_to_grasp)
         return part_to_grasp
 
-
 if __name__ == "__main__":
     hey = Vision()
     masks = glob.glob(hey.mask_path + "*")
@@ -246,10 +195,8 @@ if __name__ == "__main__":
     mask = cv2.resize(mask, dim)
     mask_contours = hey.find_contour(mask)
     x, y = hey.find_center(mask_contours)
-    z = hey.get_z(x, y, depth)
-    print(x, y, z)
-    x, y, z = hey.calibrate.calibrate(color_image, x, y, z)
-    print(x, y, z)
-    #hey.vector_normal(x, y, img, depth)
+    #x, y, z = hey.calibrate.calibrate(color_image, x, y, z)
+    #print(x, y, z)
+    SurfaceNormals.vector_normal(x, y, mask_contours, depth)
 
 
