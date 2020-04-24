c2ea0a75c091b3468653ba138a30ce9bbaff02c7
Albert <asol17@student.aau.dk>
2020-04-24 11:44:02 +0200

    Removed surface normal stuff from vision.py

--- vision/vision.py
+++ vision/vision.py
@@ -171,52 +171,6 @@ class Vision:
         print(z)
         return z
 
-    def vector_normal(self, x, y, mask, depthimage):
-        A = np.array([x, y, self.get_z(x, y, depthimage)])
-        B = np.array([x-10, y-10, self.get_z(x-10, y-10, depthimage)])
-        C = np.array([x+10, y-10, self.get_z(x+10, y-10, depthimage)])
-        D = np.array([x-10, y+10, self.get_z(x-10, y+10, depthimage)])
-
-
-        vector1 = D-B
-        vector2 = C-B
-        normal_vector = np.cross(vector1, vector2)
-        print(normal_vector)
-        """
-        #DEBUG CODE FOR VISUALISATION
-        a, b, c = normal_vector
-
-        # This evaluates a * x3 + b * y3 + c * z3 which equals d
-        d = np.dot(normal_vector, D)
-
-        print('The equation is {0}x + {1}y + {2}z = {3}'.format(a, b, c, d))
-
-        import matplotlib.pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-
-        x = np.linspace(B[0], C[0], D[0])
-        y = np.linspace(B[1], C[1], D[1])
-        X, Y = np.meshgrid(x, y)
-
-        Z = (d - a * X - b * Y) / c
-
-        # plot the mesh. Each array is 2D, so we flatten them to 1D arrays
-        ax.plot(X.flatten(),
-        Y.flatten(),
-        Z.flatten(), 'bo ')
-
-        # plot the original points. We use zip to get 1D lists of x, y and z
-        # coordinates.
-        ax.plot(*zip(B, C, D), color='r', linestyle=' ', marker='o')
-        vector_origin = B
-        ax.quiver(vector_origin[0], vector_origin[1], vector_origin[2], normal_vector[0], normal_vector[1], normal_vector[2])
-        # adjust the view so we can see the point/plane alignment
-        ax.view_init(10, 20)
-        plt.tight_layout()
-        #plt.savefig('images/plane.png')
-        plt.show()"""
 
     def find_part_for_grasp(self):
         masks = glob.glob(self.mask_path + "*")
