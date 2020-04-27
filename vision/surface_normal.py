fixed surface normal calculation

vision/surface_normal.py
@@ -1,22 +1,25 @@
 import os
 import numpy as np
 import cv2
+from PIL import Image as pimg
+import imutils
+from aruco import Calibration
 
 class SurfaceNormals:
     def __init__(self):
-        self.current_directory = os.getcwd()
-        self.mask_path = self.current_directory + "/masks/"
+        self.aruco = Calibration()
 
     def find_point_in_mask(self, centre_x, centre_y, mask_contours, point_number):
-        mask_contour = 0
+        mask_contour = None
         for c in mask_contours:
             area = cv2.contourArea(c)
-            if area < 5000:
+            if area < 1000:
                 continue
             else:
                 mask_contour = c
+        assert mask_contour.any() != None, "Couldn't find large enough mask contour"
         print(centre_x, centre_y)
-        print(mask_contour)
+        #print(mask_contour)
         max_x = 0
         max_y = 0
         for i in range(len(mask_contour)):
@@ -37,20 +40,29 @@ class SurfaceNormals:
             exit(-1)
 
     def get_z(self, x, y, depth_image):
-        z = depth_image[x,y,0]
-        print(z)
+        #TODO make finidng camera offset automatic
+        z = 1000 - depth_image[y, x, 0] * 10 #get value in mm
         return z
 
-    def vector_normal(self, x, y, mask_contours, depthimage):
-        A = np.array([x, y, self.get_z(x, y, depthimage)])
-        Bx, By = self.find_point_in_mask(x, y, mask_contours, 1)
-        B = np.array([Bx, By, self.get_z(Bx, By, depthimage)])
-        Cx, Cy = self.find_point_in_mask(x, y, mask_contours, 2)
-        C = np.array([Cx, Cy, self.get_z(Cx, Cy, depthimage)])
+    def vector_normal(self, np_mask, np_depthimage, np_reference_image):
+        # depth = image_shifter.shift_image(depth)
+        pil_depth = pimg.fromarray(np_depthimage)
+        pil_depth = pil_depth.resize((1920, 1080))
+        np_depthimage = np.asarray(pil_depth)
+        mask_contours = self.find_contour(np_mask)
+        Ax, Ay = self.find_center(mask_contours)
+        Az = self.get_z(Ax, Ay, np_depthimage)
+        A = self.aruco.calibrate(np_reference_image, Ax, Ay, Az)
+        Bx, By = self.find_point_in_mask(Ax, Ay, mask_contours, 1)
+        Bz = self.get_z(Bx, By, np_depthimage)
+        B = self.aruco.calibrate(np_reference_image, Bx, By, Bz)
+        Cx, Cy = self.find_point_in_mask(Ax, Ay, mask_contours, 2)
+        Cz = self.get_z(Cx, Cy, np_depthimage)
+        C = self.aruco.calibrate(np_reference_image, Cx, Cy, Cz)
 
         vector2 = C-A
         vector1 = B-A
-        normal_vector = np.cross(vector1, vector2)
+        normal_vector = np.cross(vector1, vector2) *-1
 
         print(normal_vector)
         """
@@ -88,3 +100,41 @@ class SurfaceNormals:
         plt.tight_layout()
         #plt.savefig('images/plane.png')
         plt.show()"""
+
+    def find_contour(self, np_mask):
+        mask = np_mask[:, :, ::-1].copy()
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+        kernel = np.ones((10, 10), np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        #cv2.imshow("MORPH", mask)
+        #cv2.waitKey(0)
+
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        return cnts
+
+    def find_center(self, cnts):
+        for c in cnts:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            # draw the contour and center of the shape on the image
+            #mask_coloured = cv2.cvtColor(mask,cv2.COLOR_GRAY2RGB)
+            # cv2.drawContours(mask_coloured, [c], -1, (0, 255, 0), 2)
+            # cv2.circle(mask_coloured, (cX, cY), 7, (255, 0, 255), -1)
+            # cv2.putText(mask_coloured, "center", (cX - 20, cY - 20),
+            # cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
+            # show the image
+            #cv2.imshow("CENTER", mask_coloured)
+            #cv2.waitKey(0)
+            return cX, cY
+
+if __name__ == "__main__":
+    sn = SurfaceNormals()
+    np_mask = np.array(pimg.open("bottom.png"))
+    np_depth = np.asarray(pimg.open("depth.png"))
+    np_reference = np.asarray(pimg.open("ref2.png"))
+    sn.vector_normal(np_mask, np_depth, np_reference)