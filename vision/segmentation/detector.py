ec054a8cb10f0000e50d9d94ca38c8b080295598
Emil Albin Ryberg <eryber17@student.aau.dk>
2020-05-05 16:08:48 +0200

    Detector for windows

--- vision/segmentation/detector.py
+++ vision/segmentation/detector.py
@@ -1,6 +1,8 @@
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
+import pkg_resources
+import os
 
 
 class InstanceDetector:
@@ -24,12 +26,16 @@ class InstanceDetector:
         cfg = get_cfg()
         try:
             cfg.merge_from_file(model_zoo.get_config_file("Misc/cascade_mask_rcnn_R_50_FPN_1x.yaml"))
-        except: #sowwy my windows install is a bit out of date
-            cfg.merge_from_file("C:/Users/Benedek/detectron2/detectron2/model_zoo/configs/Misc/cascade_mask_rcnn_R_50_FPN_1x.yaml")
+        except:  # for windows installs
+            cfg_file = pkg_resources.resource_filename(
+                "detectron2.model_zoo", os.path.join("configs", "Misc/cascade_mask_rcnn_R_50_FPN_1x.yaml")
+            )
+            cfg.merge_from_file(cfg_file)
         cfg.MODEL.WEIGHTS = weights_path
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = 5
         cfg.MODEL.RETINANET.SCORE_THRESH_TEST = 0.5
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
         cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = 0.5
+        #cfg.MODEL.DEVICE = 'cpu'
         cfg.freeze()
         return cfg
